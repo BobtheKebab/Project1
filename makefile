180415ecb570fc25d357327d1a68a2fb3c7f04c3
Kristin Lin <klin11@stuy.edu>
2017-11-26 15:46:22 -0500

    basics working

---
+++ makefile
@@ -0,0 +1,4 @@
+all: proj1.c
+	gcc proj1.c
+run: all
+	./a.out
--- proj1.c
+++ proj1.c
@@ -2,52 +2,81 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 char ** parse_args( char * line);
 int count_args(char * line);
 int run( char * line );
 
-char ** parse_args( char * line) {
-  //allocate an array with 6 slots to store char pointers in
+
+int main() {
+  char line[256];
+  printf("\nWelcome to our shell.\nType commands. Press Ctrl+C to quit.\n");
+  printf("=============================================================\n");
+
+  //continous running
+  while (1) {
+    printf("$ ");
+
+    //get user input
+    fgets(line, 256, stdin);
+
+    //remove new line from stdin
+    char * cleaned = line;
+    cleaned = strsep(&cleaned, "\n");
+    run(cleaned);
+  }
+}
+
+
+int run( char * line ) {
+
+  //put line in correct format
+  char ** args = parse_args(line);
+  int f;
+
+  //create child to run process
+  f = fork();
+  if (!f) {
+    execvp(args[0], args);
+    return 0;
+  }
+  //parent waits
+  else {
+    int status;
+    wait(&status);
+  }
+}
+
+
+char ** parse_args( char * line ) {
+
+  //allocate an array with count_args slots to store char pointers in
   char ** parsed = (char **) calloc(count_args(line), sizeof(char *));
   int index = 0;
+
   //hold the remaining line to parse; while there is more...
   char * seg = line; 
   while (seg) {
     //set the slot in parsed as the returned string segment by strsep
     parsed[index] = strsep(&seg, " ");
-     index++;
+    index++;
   }
+
   //if all slots have args, last should be NULL
   return parsed;
 }
 
+
 int count_args(char * line) {
-  int count;
-  char * seg = line;
-  while (seg) {
-    strsep(&seg, " ");
-    count++;
+  int count = 1;
+  while (*line) {
+    if (*line == ' ') {
+      count++;
+    }
+    line++;
   }
   return count;
 }
 
-int run( char * line ) {
-  
-}
-
-int main() {
-
-  char * line;
-  /*
-  read(0, &line, 256);
-  printf("%s", line);
-  */
-  fgets(&line, 256, 0);
-  /*
-    char args[500] = "ls -a -l";
-  char ** command = parse_args(args);
-  execvp(command[0], command);
-  //memory should be freed after other process takes over?
-  */
-}
