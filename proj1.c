1ed95cbff9973b1c7a8c24cd7d9c4e63cdbbff5b
BobtheKebab <ahbab.ashraf@gmail.com>
2017-11-27 21:25:34 -0500

    Started work on colon separated commands, not working yet :(

--- proj1.c
+++ proj1.c
@@ -5,8 +5,8 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-char ** parse_args( char * line);
-int count_args(char * line);
+char ** parse_args( char * line, char * mode);
+int count_args(char * line, char * mode);
 int run( char * line );
 
 
@@ -27,40 +27,56 @@ int main() {
     cleaned = strsep(&cleaned, "\n");
     run(cleaned);
   }
+  
+  /*
+  char * a = "ls -l";
+  char * b = "echo hello";
+  char ** args = {a, b};
+  run(args[0]);
+  */
 }
 
 
 int run( char * line ) {
+  char** args;
+  if (strstr(line, ";")) {
+    args = parse_args(line, ";");
+    int count = 0, target = count_args(line, ";");
+    while (count < target) {
+      run(args[count]);
+      count++;
+    }
+  } else {
+    //put line in correct format
+    args = parse_args(line, " ");
+    int f;
 
-  //put line in correct format
-  char ** args = parse_args(line);
-  int f;
-
-  //create child to run process
-  f = fork();
-  if (!f) {
-    execvp(args[0], args);
-    return 0;
-  }
-  //parent waits
-  else {
-    int status;
-    wait(&status);
+    //create child to run process
+    f = fork();
+    if (!f) {
+      execvp(args[0], args);
+      return 0;
+    }
+    //parent waits
+    else {
+      int status;
+      wait(&status);
+    }
   }
 }
 
 
-char ** parse_args( char * line ) {
-
+char ** parse_args( char * line, char * mode ) {
+  
   //allocate an array with count_args slots to store char pointers in
-  char ** parsed = (char **) calloc(count_args(line), sizeof(char *));
+  char** parsed = (char **) calloc(count_args(line, mode), sizeof(char *));
   int index = 0;
 
   //hold the remaining line to parse; while there is more...
   char * seg = line; 
   while (seg) {
     //set the slot in parsed as the returned string segment by strsep
-    parsed[index] = strsep(&seg, " ");
+    parsed[index] = strsep(&seg, mode);
     index++;
   }
 
@@ -69,10 +85,10 @@ char ** parse_args( char * line ) {
 }
 
 
-int count_args(char * line) {
+int count_args(char * line, char * mode) {
   int count = 1;
   while (*line) {
-    if (*line == ' ') {
+    if (line == mode) {
       count++;
     }
     line++;
